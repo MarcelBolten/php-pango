664415d613310deeec2c25e4ac33886e08fb7f6d
Marcel Bolten <github@marcelbolten.de>
2025-10-25 00:32:04 -0700

    missed the header

--- php_pango.h
+++ php_pango.h
@@ -43,6 +43,7 @@ extern zend_object_handlers pango_std_object_handlers;
 
 #include <pango/pango.h>
 #include <pango/pangocairo.h>
+#include <pango/pangofc-fontmap.h>
 #include "src/php_cairo_internal.h"
 
 PHP_PANGO_API extern zend_class_entry *php_pango_get_context_ce();
